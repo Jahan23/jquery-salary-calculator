ae4be25a797a91a801ca3a30eca184348ebf028a
Jahan Powell <jahanpowell23@gmail.com>
2020-05-16 09:40:30 -0500

    submit button is working

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('jquery is loaded');
-    $('#submitButton').on('click', submitEmployee);
+    $('#submit').on('click', submitEmployee);
     // $('#hello').append('Welcome to my Page!');
     // $('#hello').addClass('red');
     // $('#submit').on('click', handleSubmit);
@@ -14,18 +14,18 @@ function onReady(){
 
 let employeeArray = [];
 
-function addEmployee(){
-    console.log('addEmployee');
-    $( '#firstName' ).on( 'click', addFirstName );
-    $( '#lastName' ).on( 'click', addLastName );
-    $( '#idNumber' ).on( 'click', addIdNumber );
-    $( '#jobTitle' ).on( 'click', addJobTitle );
-    $('#annualSalary').on('click', addAnnualSalary);
+// function newEmployee(){
+//     console.log('addEmployee');
+//     $( '#firstName' ).on( 'click', addFirstName );
+//     $( '#lastName' ).on( 'click', addLastName );
+//     $( '#idNumber' ).on( 'click', addIdNumber );
+//     $( '#jobTitle' ).on( 'click', addJobTitle );
+//     $('#annualSalary').on('click', addAnnualSalary);
   
-    displayCount();
+//     displayCount();
   
   
-  }
+//   }
 
 function submitEmployee(){
     console.log('in submitEmployee');
@@ -34,7 +34,30 @@ function submitEmployee(){
         lastName: $('#lastName').val(),
         idNumber: $('#idNumber').val(),
         jobTitle: $('#jobTitle').val(),
-        annualSalary: $('#annualSalary').val()
+        annualSalary: $('#annualSalary').val(),
     }
     employeeArray.push(newEmployee);
+
+    // console.log('this the the array', employeeArray);
+    // $('#firstName').val('');
+    // $('#lastName').val('');
+    // $('#idNumber').val('');
+    // $('#jobTitle').val('');
+    // $('#annualSalary').val('');
+    // // setter
+    // // $('#wrapper').append('<button class="extraBtn">click me</button>');
+    // displayEmployee(employeeArray);
 }
+
+// function displayEmployee(arrayParam){
+//     $('#peopleList').empty();
+//     console.log("in displayPeople");
+//     for(let i = 0; i < arrayParam.length; i++){
+//         $('#peopleList').append(`
+//         <li>
+//             ${arrayParam[i].lastName}, ${arrayParam[i].firstName}
+//             <button class="deleteBtn">delete</button>
+//         </li>
+//         `)
+//     }
+// }
